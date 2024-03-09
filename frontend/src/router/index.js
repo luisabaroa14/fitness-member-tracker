e9271743b7dd39cb76d0b9aeb265312c9cf33322
Luis Abaroa <labaroasordo@gmail.com>
2024-03-09 10:49:54 -0600

    feat: Not Found View

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,41 +5,75 @@ import UsersView from '@/views/UsersView.vue'
 import EditUserView from '@/views/EditUserView.vue'
 import AddPaymentView from '@/views/AddPaymentView.vue'
 import AddClassView from '@/views/AddClassView.vue'
+import LogInView from '@/views/LogInView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import { isAuthenticated } from '@/utils/loginStatus'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/login',
+      component: LogInView
+    },
+    {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: { requiresAuth: true }
     },
     {
       path: '/create-user',
       name: 'create-user',
-      component: CreateUserView
+      component: CreateUserView,
+      meta: { requiresAuth: true }
     },
     {
       path: '/users',
       name: 'users',
-      component: UsersView
+      component: UsersView,
+      meta: { requiresAuth: true }
     },
     {
       path: '/edit-user/:id',
+      name: 'edit-user',
       component: EditUserView,
-      name: 'edit-user'
+      meta: { requiresAuth: true }
     },
     {
       path: '/add-payment/:id',
+      name: 'add-payment',
       component: AddPaymentView,
-      name: 'add-payment'
+      meta: { requiresAuth: true }
     },
     {
       path: '/add-class/:id',
+      name: 'add-class',
       component: AddClassView,
-      name: 'add-class'
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/not-found',
+      name: 'not-found',
+      component: NotFoundView,
+      meta: { requiresAuth: true }
+    },
+    // Catch-all route for any other routes that are not found
+    {
+      path: '/:catchAll(.*)',
+      redirect: '/not-found' 
     }
   ]
+})
+
+// Navigation guard to check if the route requires authentication
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && isAuthenticated() === false) {
+    next('/login')
+  } else {
+    // Proceed to the requested route
+    next()
+  }
 })
 
 export default router
---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>404 Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+    <button @click="$router.push('/')">Go to Home</button>
+  </div>
+</template>
