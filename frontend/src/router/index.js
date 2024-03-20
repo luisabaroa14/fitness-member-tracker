8ece2e795f817b278fd228461f4fb80fbaf38652
Luis Abaroa <labaroasordo@gmail.com>
2024-03-19 20:34:16 -0600

    refactor: Split admin and staff accounts

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,7 +7,7 @@ import AddPaymentView from '@/views/AddPaymentView.vue'
 import AddClassView from '@/views/AddClassView.vue'
 import LogInView from '@/views/LogInView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
-import { isAuthenticated } from '@/utils/loginStatus'
+import { isAdminAuthenticated, isStaffAuthenticated } from '@/utils/loginStatus'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,59 +20,65 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-      meta: { requiresAuth: true }
+      meta: { adminOnly: true }
     },
     {
       path: '/create-user',
       name: 'create-user',
       component: CreateUserView,
-      meta: { requiresAuth: true }
+      meta: { adminOnly: true }
     },
     {
       path: '/users',
       name: 'users',
       component: UsersView,
-      meta: { requiresAuth: true }
+      meta: { adminOnly: true }
     },
     {
       path: '/edit-user/:id',
       name: 'edit-user',
       component: EditUserView,
-      meta: { requiresAuth: true }
+      meta: { adminOnly: true }
     },
     {
       path: '/add-payment/:id',
       name: 'add-payment',
       component: AddPaymentView,
-      meta: { requiresAuth: true }
+      meta: { adminOnly: true }
     },
     {
       path: '/add-class/:id',
       name: 'add-class',
       component: AddClassView,
-      meta: { requiresAuth: true }
+      meta: { adminOnly: false }
     },
     {
       path: '/not-found',
       name: 'not-found',
       component: NotFoundView,
-      meta: { requiresAuth: true }
+      meta: { adminOnly: true }
     },
     // Catch-all route for any other routes that are not found
     {
       path: '/:catchAll(.*)',
-      redirect: '/not-found' 
+      redirect: '/not-found'
     }
   ]
 })
 
 // Navigation guard to check if the route requires authentication
 router.beforeEach((to, from, next) => {
-  if (to.meta.requiresAuth && isAuthenticated() === false) {
-    next('/login')
-  } else {
-    // Proceed to the requested route
+  // Allow access to the login page
+  if (to.path === '/login') {
     next()
+  } else {
+    // Check if the user has admin access or staff access to non-admin routes
+    if (isAdminAuthenticated() || (isStaffAuthenticated() && !to.meta.adminOnly)) {
+      next()
+    } else {
+      // Redirect to the login page for unauthorized access
+      next('/login')
+    }
   }
 })
 
