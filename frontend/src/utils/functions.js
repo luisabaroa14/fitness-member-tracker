d255ec86b51938f5d00b876386b1a0668e7398f3
Luis Abaroa <labaroasordo@gmail.com>
2024-03-19 19:19:41 -0600

    feat: Format date function

--- frontend/src/utils/functions.js
+++ frontend/src/utils/functions.js
@@ -1,6 +1,21 @@
-
 /*
  * Function to get the current date in the format YYYY-MM-DD
-*/
+ */
 export const maxInputDate = () =>
   new Date(new Date().getTime() - 24 * 60 * 60 * 1000).toISOString().split('T')[0]
+
+/*
+ * Function to format the date and time in the specified format.
+ * @param {Date} date - The date object to be formatted.
+ * @returns {string} - Formatted date and time string.
+ */
+export const formatDate = (date) => {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+  }
+  return new Date(date).toLocaleDateString('en-US', options)
+}
